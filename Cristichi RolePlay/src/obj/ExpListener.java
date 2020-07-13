64cd195bb8d61cbb4d80768635cbd7f0da4b4aba
CristobalPerezDAMW <cristobalperez.dam@gmail.com>
2020-07-13 11:50:26 +0200

    Improved Archer levelup

--- Cristichi RolePlay/src/obj/ExpListener.java
+++ Cristichi RolePlay/src/obj/ExpListener.java
@@ -4,11 +4,9 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.FishHook;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
 import org.bukkit.entity.SpectralArrow;
 import org.bukkit.entity.ThrowableProjectile;
 import org.bukkit.entity.Trident;
@@ -62,22 +60,25 @@ public class ExpListener implements Listener {
 
 		if (offender instanceof Projectile) {
 			if (offender.hasMetadata(META_ARROW_ARCHER)) {
+				String projectileName = offender.getName();
 				String offenderName = offender.getMetadata(META_ARROW_ARCHER).get(0).asString();
 				Player p = plugin.getServer().getPlayer(offenderName);
 				if (p != null) {
 					Stats stats = StatsPlayer.players.get(p.getUniqueId());
 					double prob = 0.1;
-					if (offender instanceof FishHook) {
-						prob = 0.05;
-					} else if (offender instanceof Arrow) {
+					if (offender instanceof Arrow) {
 						prob = 0.2;
 						if (offender.hasMetadata(META_ARROW_FORCE)) {
 							prob *= offender.getMetadata(META_ARROW_FORCE).get(0).asFloat();
 						}
-					} else if (offender instanceof SpectralArrow || offender instanceof Trident) {
+					} else if (offender instanceof SpectralArrow) {
+						prob = 0.3;
+						projectileName = "Spectral Arrow";
+					} else if ( offender instanceof Trident) {
 						prob = 0.3;
 					} else if (offender instanceof EnderPearl) {
 						prob = 0.4;
+						projectileName = "Ender Pearl";
 					} else if (offender instanceof ThrowableProjectile) {
 						prob = 0.115;
 					}
@@ -86,7 +87,7 @@ public class ExpListener implements Listener {
 						stats.increaseExp(1);
 						StatsPlayer.players.put(p.getUniqueId(), stats);
 						p.sendMessage(plugin.header + "You gained 1 exp for hitting " + defender.getName() + " with "
-								+ offender.getName() + "!");
+								+ projectileName + "!");
 						p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 0);
 						offender = p;
 					}
